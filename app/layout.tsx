9a8d03c5ee5167b37e8c62869d726ec3276b0661
Lucas Aschenbach <Lucas.Aschenbach@gmail.com>
2024-03-09 01:28:12 +0100

    Skip restore cache on rebuild (temp)

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,9 +22,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );

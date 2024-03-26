8badbd67f9bb5895890f9474869e963ed856820b
KameterLuis <contact@luiskameter.com>
2024-03-26 11:18:02 +0100

    Removed possible deployment error in head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,12 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script async src="https://tally.so/widgets/embed.js" />
-        <script
-          id="cookieyes"
-          type="text/javascript"
-          src="https://cdn-cookieyes.com/client_data/c872b6f1b2d2d50968a58838/script.js"
-        ></script>
+        <Script src="https://tally.so/widgets/embed.js"></Script>
+        <Script
+          src={`https://cdn-cookieyes.com/client_data/c872b6f1b2d2d50968a58838/script.js`}
+          strategy="beforeInteractive"
+        ></Script>
       </head>
       <body className={inter.className}>{children}</body>
       <script

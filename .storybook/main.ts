fa8f8341ba8628487b513004db7d759a30018ddf
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-04-21 00:24:47 +0200

    change: removed chromatic

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,14 +2,12 @@ import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../app/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y",
   ],

a9eb3ee7f41f3b4f6cd35f760332a705487a86b9
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-04-20 22:08:00 +0200

    chore: run prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,9 +10,9 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        'tbc-yellow': '#FFCB6D',
-        'tbc-pink': '#E434A5',
-        'tbc-blue': '#348AEB',
+        "tbc-yellow": "#FFCB6D",
+        "tbc-pink": "#E434A5",
+        "tbc-blue": "#348AEB",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -20,10 +20,10 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       screens: {
-        'mobile-wide': {
-          'raw': '((max-width: 1020px) and (max-height: 620px))'
+        "mobile-wide": {
+          raw: "((max-width: 1020px) and (max-height: 620px))",
         },
-      }
+      },
     },
   },
   plugins: [],

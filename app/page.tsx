f6f18a3c587dd928877fccbffb1d2c71a2e1a047
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-13 19:43:38 +0200

    change: header and footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
 "use client";
 
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import { Footer } from "@/app/components/footer/Footer";
 import Canvas from "./components/Canvas";
 import Hero from "./sections/Hero";
 import Manifesto from "./sections/Manifesto";
 import { Button } from "@/app/components/button";
+import { Header } from "@/app/components/header";
 
 export default function Home() {
   return (
     <div className="flex justify-center w-full">
       <Canvas />
       <div className="w-full max-w-[1500px]">
-        <Navbar />
+        <Header />
         <Hero />
         <Manifesto />
         <div

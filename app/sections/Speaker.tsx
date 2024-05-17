6f278a11b0bf3053ea3ae486407b53354f1105a5
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-17 21:25:07 +0200

    fix: speakers size and content

--- app/sections/Speaker.tsx
+++ app/sections/Speaker.tsx
@@ -9,15 +9,15 @@ const Speaker = () => {
       <Text textType={"sub_hero"} className="text-gradient">
         Speakers
       </Text>
-      <div className={"grid grid-cols-4 gap-x-4 gap-y-12"}>
-      <SpeakerComponent imageSrc={""} name="Hilmar M. Orth" position="Founder at Gelato and Arrakis" company=""/>
-      <SpeakerComponent imageSrc={""} name="Leonardo Alt" position="Formal Verification Lead. Compiler Engineer at Ethereum Foundation" company=""/>
-      <SpeakerComponent imageSrc={""} name="Sarah Gramberg" position="Supervisory Expert Crypto-Assets at European Banking Authority" company=""/>
-      <SpeakerComponent imageSrc={""} name="Marina Khaustova" position="CEO at Crytal Blockchain Analytics" company=""/>
-      <SpeakerComponent imageSrc={""} name="Andri Rabetanety" position="Chief Operating Officer 1inch Network" company=""/>
-      <SpeakerComponent imageSrc={""} name="Alessandro Ferrari" position="Head of Product of Algorand Foundation" company=""/>
-      <SpeakerComponent imageSrc={""} name="Prof. Dr. Philipp Maume" position="Professor of Corporate Governance and Capital Markets Law at TUM" company=""/>
-      <SpeakerComponent imageSrc={""} name="Philipp Lesche" position="Advisor for Digital Technologies & Head of Bavarian Center for Blockchain" company=""/>
+      <div className={"flex flex-wrap justify-center gap-x-4 gap-y-12"}>
+      <SpeakerComponent imageSrc={"https://upload.wikimedia.org/wikipedia/commons/1/1c/Vitalik_Buterin_TechCrunch_London_2015_%28cropped%29.jpg"} name="Hilmar M. Orth" position="Founder" company="Gelato and Arrakis"/>
+      <SpeakerComponent imageSrc={"https://upload.wikimedia.org/wikipedia/commons/1/1c/Vitalik_Buterin_TechCrunch_London_2015_%28cropped%29.jpg"} name="Leonardo Alt" position="Formal Verification Lead. Compiler Engineer" company="Ethereum Foundation"/>
+      <SpeakerComponent imageSrc={"https://upload.wikimedia.org/wikipedia/commons/1/1c/Vitalik_Buterin_TechCrunch_London_2015_%28cropped%29.jpg"} name="Sarah Gramberg" position="Supervisory Expert Crypto-Assets" company="European Banking Authority"/>
+      <SpeakerComponent imageSrc={"https://upload.wikimedia.org/wikipedia/commons/1/1c/Vitalik_Buterin_TechCrunch_London_2015_%28cropped%29.jpg"} name="Marina Khaustova" position="Chief Executive Officer" company="Crytal Blockchain Analytics"/>
+      <SpeakerComponent imageSrc={"https://upload.wikimedia.org/wikipedia/commons/1/1c/Vitalik_Buterin_TechCrunch_London_2015_%28cropped%29.jpg"} name="Andri Rabetanety" position="Chief Operating Officer" company="1inch Network"/>
+      <SpeakerComponent imageSrc={"https://upload.wikimedia.org/wikipedia/commons/1/1c/Vitalik_Buterin_TechCrunch_London_2015_%28cropped%29.jpg"} name="Alessandro Ferrari" position="Head of Product" company="Algorand Foundation"/>
+      <SpeakerComponent imageSrc={"https://upload.wikimedia.org/wikipedia/commons/1/1c/Vitalik_Buterin_TechCrunch_London_2015_%28cropped%29.jpg"} name="Prof. Dr. Philipp Maume" position="Professor of Corporate Governance and Capital Markets Law" company="TUM"/>
+      <SpeakerComponent imageSrc={"https://upload.wikimedia.org/wikipedia/commons/1/1c/Vitalik_Buterin_TechCrunch_London_2015_%28cropped%29.jpg"} name="Philipp Lesche" position="Advisor" company="Digital Technologies & Head of Bavarian Center for Blockchain"/>
       </div>
       <div className="w-full flex justify-center">
         <Button buttonType={"primary"} asChild>

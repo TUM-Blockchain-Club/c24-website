bcbd2053c21bb649c75f8368e23b74f8d5dfc32c
luis <luis.kameter@tum.de>
2024-05-17 15:41:09 +0200

    Add: Speaker Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import Navbar from "./components/Navbar";
 import { Footer } from "@/app/components/footer/Footer";
 import Canvas from "./components/Canvas";
 import Hero from "./sections/Hero";
 import Manifesto from "./sections/Manifesto";
-import { Button } from "@/app/components/button";
 import { Header } from "@/app/components/header";
+import { Button } from "@/app/components/button";
+import Speaker from "./sections/Speaker";
 
 export default function Home() {
   return (
@@ -18,13 +18,7 @@ export default function Home() {
           <Hero />
           <div className={"flex flex-col pb-24 gap-32"}>
             <Manifesto />
-            <div
-              className={"w-full mt-20 mb-40 px-12 2xl:px-0 flex justify-center"}
-            >
-              <Button buttonType={"primary"} asChild>
-                <a href={"https://tally.so/r/wk62O6"}>Apply as speaker</a>
-              </Button>
-            </div>
+            <Speaker />
           </div>
         </div>
       </main>

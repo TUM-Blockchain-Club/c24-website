b208ace1e6fc5d118c99ca038a5933b0c572a224
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-17 21:07:09 +0200

    add: container

---
+++ app/components/container/Container.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import classNames from "classnames";
+
+type ContainerElement = React.ElementRef<"div">;
+export type ContainerProps = React.ComponentPropsWithoutRef<"div">;
+
+export const Container = React.forwardRef<ContainerElement, ContainerProps>(
+  (props, ref) => {
+    const { className, ...restProps } = props;
+    return (
+      <div
+        {...restProps}
+        className={classNames(className, "px-6 md:px-12 lg:px-24")}
+      ></div>
+    );
+  },
+);
+Container.displayName = "Container";

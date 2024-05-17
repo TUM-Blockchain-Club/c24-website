e89eb96455936d3b5e7b3db27fd41f4e88965dd3
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-17 21:14:14 +0200

    fix: speaker

--- app/sections/Speaker.tsx
+++ app/sections/Speaker.tsx
@@ -5,13 +5,11 @@ import { Button } from "@/app/components/button";
 
 const Speaker = () => {
   return (
-    <section className="w-full px-12 2xl:px-0" id="speaker">
-      <div className="w-full flex justify-center">
-        <Text textType={"sub_hero"} className="text-gradient">
-          Speakers
-        </Text>
-      </div>
-      <div>
+    <section className="w-full flex flex-col items-center gap-32" id="speaker">
+      <Text textType={"sub_hero"} className="text-gradient">
+        Speakers
+      </Text>
+      <div className={"grid grid-cols-4 gap-x-4 gap-y-12"}>
       <SpeakerComponent imageSrc={""} name="Hilmar M. Orth" position="Founder at Gelato and Arrakis" company=""/>
       <SpeakerComponent imageSrc={""} name="Leonardo Alt" position="Formal Verification Lead. Compiler Engineer at Ethereum Foundation" company=""/>
       <SpeakerComponent imageSrc={""} name="Sarah Gramberg" position="Supervisory Expert Crypto-Assets at European Banking Authority" company=""/>

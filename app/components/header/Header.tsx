95e1a8487a54a616e4ccc6c696d13cda46a4c4f9
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-18 11:22:08 +0200

    change: transparent header when not scrolled

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -1,24 +1,49 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import classNames from "classnames";
 import Image from "next/image";
-import confLogo from "@/public/logos/tbc-conference-logo.png";
 import { Text } from "@/app/components/text";
 import { Button } from "@/app/components/button";
 import { Link } from "@/app/components/link";
 import NextLink from "next/link";
 
 type HeaderElement = React.ElementRef<"header">;
-export type HeaderProps = React.ComponentPropsWithoutRef<"header">;
+export type HeaderProps = React.ComponentPropsWithoutRef<"header"> & {
+  logoUrl?: string;
+};
 
 export const Header = React.forwardRef<HeaderElement, HeaderProps>(
   (props, ref) => {
-    const { className, ...propRest } = props;
+    const { className, logoUrl, ...propRest } = props;
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+      const handleScroll = () => {
+        if (window.scrollY > 100) {
+          setIsScrolled(true);
+        } else {
+          setIsScrolled(false);
+        }
+      };
+
+      window.addEventListener("scroll", handleScroll);
+
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }, []);
+
     return (
       <header
         {...propRest}
         className={classNames(
           className,
-          "fixed z-[9999] dark:bg-black/50 scroll:bg-black/75 w-full py-4 flex justify-center items-center px-6 md:px-12 lg:px-24 backdrop-blur-md",
+          "fixed z-[9999] w-full py-4 flex justify-center items-center px-6 md:px-12 lg:px-24 transition-all duration-300",
+          {
+            "dark:bg-black/50 backdrop-blur-md": isScrolled,
+            "bg-transparent": !isScrolled,
+          },
         )}
         ref={ref}
       >
@@ -26,7 +51,7 @@ export const Header = React.forwardRef<HeaderElement, HeaderProps>(
           <div className={"w-11 lg:w-16"}>
             <NextLink href={"/"}>
               <Image
-                src={confLogo}
+                src={logoUrl || "/logos/tbc-conference-logo.png"}
                 alt={"TUM Blockchain Conference 2024 Logo"}
                 width={48}
                 height={48}

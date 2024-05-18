04a4ac9ae23bd8277fbd3d05224e973f8e5c4b52
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-18 11:29:46 +0200

    fix: remove duplicate speaker application button

--- app/page.tsx
+++ app/page.tsx
@@ -23,15 +23,6 @@ export default function Home() {
             <div className={"flex flex-col pb-24 gap-32"}>
               <Statistic />
               <Manifesto />
-              <div
-                className={
-                  "w-full mt-20 mb-40 px-12 2xl:px-0 flex justify-center"
-                }
-              >
-                <Button buttonType={"primary"} asChild>
-                  <a href={"https://tally.so/r/wk62O6"}>Apply as speaker</a>
-                </Button>
-              </div>
               <Speaker />
             </div>
           </div>

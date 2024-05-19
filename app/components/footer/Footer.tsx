dc5974de2c4b35d96fa53b53eef7b084015f5e66
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-19 23:22:50 +0200

    fix: background image mirrored when screen to big

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -18,65 +18,83 @@ export const Footer = React.forwardRef<FooterElement, FooterProps>(
         {...restProps}
         className={classNames(
           className,
-          "flex flex-col lg:block w-full relative min-h-[500px] z-10",
+          "flex z-10 justify-center items-center justify-items-center",
         )}
         ref={ref}
       >
-        <div className={"flex justify-center py-6 px-6 md:px-12 lg:px-24 z-20"}>
+        <div
+          className={
+            "flex flex-col self-center relative min-h-[500px] w-full max-w-7xl"
+          }
+        >
           <div
-            className={
-              "flex flex-col gap-y-8 lg:flex-row justify-between max-w-7xl w-full"
-            }
+            className={"flex justify-center py-6 px-6 md:px-12 lg:px-24 z-20"}
           >
-            <div className={"flex flex-col gap-4 items-start"}>
-              <Image
-                src={confLogo}
-                alt={"TUM Blockchain Conference Logo"}
-                height={88}
-              />
-              <div className={"flex flex-col gap-2"}>
-                <Text textType={"small"}>Organized by</Text>
+            <div
+              className={
+                "flex flex-col gap-y-8 lg:flex-row justify-between w-full"
+              }
+            >
+              <div className={"flex flex-col gap-4 items-start"}>
                 <Image
-                  src={tbcLogo}
-                  alt={"TUM Blockchain Club Logo"}
-                  width={145}
+                  src={confLogo}
+                  alt={"TUM Blockchain Conference Logo"}
+                  height={88}
                 />
+                <div className={"flex flex-col gap-2"}>
+                  <Text textType={"small"}>Organized by</Text>
+                  <Image
+                    src={tbcLogo}
+                    alt={"TUM Blockchain Club Logo"}
+                    width={145}
+                  />
+                </div>
               </div>
+              <div
+                className={
+                  "flex flex-col lg:flex-row h-full justify-start lg:justify-center gap-4 lg:gap-8"
+                }
+              >
+                <Text asChild>
+                  <Link href={"mailto:relations@tum-blockchain.com"}>
+                    Contact
+                  </Link>
+                </Text>
+                <Text asChild>
+                  <Link href={"https://www.tum-blockchain.com/privacy-policy"}>
+                    Privacy Policy
+                  </Link>
+                </Text>
+                <Text asChild>
+                  <Link href={"https://www.tum-blockchain.com/imprint"}>
+                    Impressum
+                  </Link>
+                </Text>
+              </div>
+            </div>
+          </div>
+          <div className={"self-end block max-w-7xl w-full h-[420px] lg:h-fit"}>
+            <div className={"absolute bottom-0 z-10 right-0"}>
+              <Image
+                src={munich}
+                alt={"Munich"}
+                height={420}
+                className={"mix-blend-screen"}
+              />
             </div>
             <div
               className={
-                "flex flex-col lg:flex-row h-full justify-start lg:justify-center gap-4 lg:gap-8"
+                "lg:absolute bottom-0 z-10 right-0 translate-x-[100%] -scale-x-[100%] hidden xl:block"
               }
             >
-              <Text asChild>
-                <Link href={"mailto:relations@tum-blockchain.com"}>
-                  Contact
-                </Link>
-              </Text>
-              <Text asChild>
-                <Link href={"https://www.tum-blockchain.com/privacy-policy"}>
-                  Privacy Policy
-                </Link>
-              </Text>
-              <Text asChild>
-                <Link href={"https://www.tum-blockchain.com/imprint"}>
-                  Impressum
-                </Link>
-              </Text>
+              <Image
+                src={munich}
+                alt={"Munich"}
+                height={420}
+                className={"mix-blend-screen"}
+              />
             </div>
           </div>
-        </div>
-        <div
-          className={
-            "self-end block max-w-[80%] max-h-[420px] lg:absolute bottom-0 z-10 right-0"
-          }
-        >
-          <Image
-            src={munich}
-            alt={"Munich"}
-            height={420}
-            className={"mix-blend-screen"}
-          />
         </div>
       </footer>
     );

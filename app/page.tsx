ed25ad81f06efa723d834011ca12161afd5cdbbb
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-05-19 23:23:25 +0200

    change: hid sponsor section

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,16 @@ import Statistic from "@/app/sections/Statistic";
 import Sparkle from "@/app/components/Sparkle";
 import { Button } from "@/app/components/button";
 import Speaker from "./sections/Speaker";
+import Sponsors from "@/app/sections/Sponsors";
 
 export default function Home() {
   return (
-    <div>
+    <div className={"overflow-x-hidden"}>
       <Header />
       <Sparkle />
       <main className={"w-full flex justify-center pt-[25px] lg:pt-0 z-20"}>
         <Container>
-          <div className={"flex flex-col max-w-7xl"}>
+          <div className={"flex flex-col max-w-7xl z-10"}>
             <Hero />
             <div className={"flex flex-col pb-24 gap-32"}>
               <Statistic />

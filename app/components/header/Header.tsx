088a80738b1de3e6d6b43e71613920b4b93960d3
KameterLuis <contact@luiskameter.com>
2024-07-08 09:10:05 +0200

    [Fix] commented out code

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -51,7 +51,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
           <Text asChild>
             <Link href="#sponsors">Sponsors</Link>
           </Text>
-          <Text asChild>
+          {/*<Text asChild>
             <Link href="#grants">Student Grants</Link>
           </Text>
           <Text asChild>
@@ -59,7 +59,7 @@ export const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
           </Text>
           <Text asChild>
             <Link href="#faq">FAQ</Link>
-          </Text>
+          </Text>*/}
         </nav>
         <Button asChild className={"sm:hidden mt-8 mr-4"}>
           <NextLink href="https://tum-blockchain-conference-24.eventbrite.de/">
@@ -136,7 +136,7 @@ export const Header = React.forwardRef<HeaderElement, HeaderProps>(
               </Text>
               {/*<Text asChild>
                 <Link href="#grants">Student Grants</Link>
-              </Text>*/}
+              </Text>
               <Text asChild>
                 <Link href="#faq">FAQ</Link>
               </Text>*/}

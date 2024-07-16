0ccf3170af478a0ab93a46c7f44f0d680bd0ee14
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-07-16 18:07:38 +0200

    change: enlarged ticket on mobile device

--- app/sections/Tickets.tsx
+++ app/sections/Tickets.tsx
@@ -11,7 +11,7 @@ const Tickets = () => {
         </Text>
       </div>
       <div className="w-full flex justify-center">
-        <div className="sm:flex mt-20 justify-center sm:space-x-10 space-y-10 sm:space-y-0 w-[175px] sm:w-full">
+        <div className="sm:flex mt-20 justify-center sm:space-x-10 space-y-10 sm:space-y-0 w-7/8 sm:w-full">
           <Ticket price={10} ticketType={"student"} title="Student" />
           <Ticket price={129} ticketType={"regular"} title="Regular" />
           <Ticket price={499} ticketType={"vip"} title="VIP" />

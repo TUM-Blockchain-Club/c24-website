21eb10a8c886758ddce41800280e1ac8545f3525
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-08-11 11:29:13 +0200

    add: community partners

--- app/sections/Partners.tsx
+++ app/sections/Partners.tsx
@@ -3,28 +3,51 @@ import { Text } from "../components/text";
 
 const Partners = () => {
   return (
-    <section className="w-full" id="tickets">
-      <Text className="w-full text-center" as="p" textType={"sub_title"}>
-        Knowledge Partner
-      </Text>
-      <div className="w-full flex justify-center md:mt-10">
-        <div className="relative w-[150px] h-[100px]">
-          <Image
-            alt="IEEE Blockchain"
-            src="/papers/ieee_logo.png"
-            fill={true}
-            style={{ objectFit: "contain" }}
-          ></Image>
-        </div>
+    <section className="w-full flex flex-col items-center gap-12 lg:gap-20">
+      <div className={"w-full flex justify-center"}>
+        <Text textType={"sub_hero"} className="text-gradient">
+          Partners
+        </Text>
       </div>
-      <Text className="mt-20 w-full text-center" as="p" textType={"sub_title"}>
-        Media Partners
-      </Text>
-      <div className="w-full flex justify-center md:mt-10">
-        <div className="sm:flex sm:space-x-8">
-          <div className="relative w-[155px] h-[100px]">
+      <div className={"flex flex-col gap-6 lg:gap-12"}>
+        <Text className="w-full text-center" as="p" textType={"sub_title"}>
+          Knowledge Partner
+        </Text>
+        <div className="w-full flex justify-center">
+          <div className="relative w-[150px] h-[100px]">
             <Image
               alt="IEEE Blockchain"
+              src="/papers/ieee_logo.png"
+              fill={true}
+              style={{ objectFit: "contain" }}
+            ></Image>
+          </div>
+        </div>
+      </div>
+      <div className={"flex flex-col gap-6 lg:gap-12"}>
+        <Text className="w-full text-center" as="p" textType={"sub_title"}>
+          Media Partners
+        </Text>
+        <div className="w-full flex justify-center gap-8 flex-wrap">
+          <div className="relative w-[155px] h-[100px]">
+            <Image
+              alt="BTC Echo"
+              src="/partners/btc_echo_logo.png"
+              fill={true}
+              style={{ objectFit: "contain" }}
+            ></Image>
+          </div>
+          <div className="relative w-[150px] h-[100px]">
+            <Image
+              alt="Superteam Germany"
+              src="/partners/superteam_de_logo.png"
+              fill={true}
+              style={{ objectFit: "contain" }}
+            ></Image>
+          </div>
+          <div className="relative w-[155px] h-[100px]">
+            <Image
+              alt="The Coin Republic"
               src="/partners/tcr_logo.png"
               fill={true}
               style={{ objectFit: "contain" }}
@@ -32,7 +55,7 @@ const Partners = () => {
           </div>
           <div className="relative w-[155px] h-[100px]">
             <Image
-              alt="IEEE Blockchain"
+              alt="The Market Periodical"
               src="/partners/tmp_logo.png"
               fill={true}
               style={{ objectFit: "contain" }}
@@ -40,20 +63,41 @@ const Partners = () => {
           </div>
         </div>
       </div>
-      <div className="w-full flex justify-center md:mt-10">
-        <div className="sm:flex sm:space-x-8">
-          <div className="relative w-[155px] h-[100px]">
+      <div className={"flex flex-col gap-6 lg:gap-12"}>
+        <Text className="w-full text-center" as="p" textType={"sub_title"}>
+          Community Partners
+        </Text>
+        <div className="w-full flex justify-center gap-8 flex-wrap">
+          <div className="relative w-[155px] h-[100px] flex items-center justify-center">
             <Image
-              alt="IEEE Blockchain"
-              src="/partners/btc_echo_logo.png"
+              alt="Technische Universität München"
+              src="/partners/tum_logo.png"
+              width={110}
+              height={0}
+              style={{ objectFit: "contain" }}
+            ></Image>
+          </div>
+          <div className="relative w-[155px] h-[100px] flex items-center justify-center">
+            <Image
+              alt="Encode Club"
+              src="/partners/encode_logo.png"
+              width={110}
+              height={0}
+              style={{ objectFit: "contain" }}
+            ></Image>
+          </div>
+          <div className="relative w-[150px] h-[100px]">
+            <Image
+              alt="Blockchain Bayern e.V."
+              src="/partners/blockchain_bayern_logo.png"
               fill={true}
               style={{ objectFit: "contain" }}
             ></Image>
           </div>
-          <div className="relative w-[150px] h-[100px] flex">
+          <div className="relative w-[155px] h-[100px]">
             <Image
-              alt="IEEE Blockchain"
-              src="/partners/superteam_de_logo.png"
+              alt="Blockchain Bundesverband (Bundesblock)"
+              src="/partners/bundesblock_logo.png"
               fill={true}
               style={{ objectFit: "contain" }}
             ></Image>

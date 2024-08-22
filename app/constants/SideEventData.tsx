4bccd2c6e821bde750ee6fb857f06ffb2c765ea1
KameterLuis <contact@luiskameter.com>
2024-08-22 20:15:00 +0200

    [Fix] Removed async for staticParams

--- app/constants/SideEventData.tsx
+++ app/constants/SideEventData.tsx
@@ -2,7 +2,7 @@ export const sideEventsItems = [
   {
     title: "XRPL Meetup in Munich",
     url: "xrpl-meetup",
-    subpage: false,
+    subpage: true,
     description:
       "XRPL Meetups aim to spread knowledge and create real-life connections and communities around blockchain and the XRP Ledger by establishing local “XRPL Hubs” across Europe. These meetups provide regular gatherings where individuals from various sectors can meet, exchange ideas, and deepen their understanding of blockchain technology. Designed to be inclusive, XRPL Meetups welcome participants from academia, private industries, public sectors, and NGOs.",
     link: "https://www.eventbrite.fr/e/xrpl-meetup-in-munich-tickets-957654278727",

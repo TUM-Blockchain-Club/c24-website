2f7185e31b90dfeb3da4b1d2a4ad76e3614a52b7
KameterLuis <contact@luiskameter.com>
2024-08-22 20:09:51 +0200

    [Edit] Side Event Build failure

--- app/components/event/Event.tsx
+++ app/components/event/Event.tsx
@@ -3,9 +3,9 @@
 import { Text } from "@/app/components/text";
 import classNames from "classnames";
 import Image from "next/image";
+import NextLink from "next/link";
 import React from "react";
 import { Button } from "../button";
-import NextLink from "next/link";
 
 interface EventModel {
   title: string;
@@ -36,43 +36,33 @@ export const Event = React.forwardRef<EventElement, EventProps>((props) => {
     <div
       className={classNames(
         className,
-        "md:flex cta-border border-[1px] border-white p-6 mt-10 hover:scale-[102%] duration-500 ease-in-out",
+        "flex flex-col cta-border border-[1px] border-white p-6 mt-10 hover:scale-[102%] duration-500 ease-in-out",
       )}
       {...restProps}
     >
-      <div>
-        <div className="relative w-full h-[200px]">
-          <Image
-            className={"object-contain pl-4 md:hidden"}
-            src={bgImage}
-            alt={title}
-            title={title}
-            fill
-          />
-        </div>
-        <Text textType={"sub_title"} as="p" className="mt-">
-          {title}
-        </Text>
-        <Text className="mt-4 underline" textType={"small"} as="p">
-          {date}
-        </Text>
-        <Text className="mt-4" textType={"small"} as="p">
-          {description}
-        </Text>
-        <NextLink href={subpage ? `/side-events/${url}` : link}>
-          <Button className="mt-4" buttonType={"primary"}>
-            more
-          </Button>
-        </NextLink>
+      <div className="relative w-full h-[200px]">
+        <Image
+          className={"object-contain pl-4"}
+          src={bgImage}
+          alt={title}
+          title={title}
+          fill
+        />
       </div>
-      <Image
-        className={"object-cover pl-4 hidden md:block"}
-        src={bgImage}
-        alt={title}
-        title={title}
-        width={275}
-        height={275}
-      />
+      <Text textType={"sub_title"} as="p" className="mt-10">
+        {title}
+      </Text>
+      <Text className="mt-4 underline" textType={"small"} as="p">
+        {date}
+      </Text>
+      <Text className="mt-4" textType={"small"} as="p">
+        {description}
+      </Text>
+      <NextLink href={subpage ? `/side-events/${url}` : link}>
+        <Button className="mt-4" buttonType={"primary"}>
+          more
+        </Button>
+      </NextLink>
     </div>
   );
 });
--- app/constants/SideEventData.tsx
+++ app/constants/SideEventData.tsx
@@ -7,6 +7,16 @@ export const sideEventsItems = [
       "XRPL Meetups aim to spread knowledge and create real-life connections and communities around blockchain and the XRP Ledger by establishing local “XRPL Hubs” across Europe. These meetups provide regular gatherings where individuals from various sectors can meet, exchange ideas, and deepen their understanding of blockchain technology. Designed to be inclusive, XRPL Meetups welcome participants from academia, private industries, public sectors, and NGOs.",
     link: "https://www.eventbrite.fr/e/xrpl-meetup-in-munich-tickets-957654278727",
     date: "Thursday, September 12 | 5:30 - 9pm",
-    backgroundImg: "/venue/venue_2.jpg",
+    backgroundImg: "/side-events/xrpl-meetup.jpg",
+  },
+  {
+    title: "Solana Mixer Munich",
+    url: "solana-mixer",
+    subpage: false,
+    description:
+      "​Join us for our TUM Blockchain Conference side event together with Aachen Blockchain Club.",
+    link: "https://lu.ma/rt4yg9gp",
+    date: "Wednesday, September 11 | 6:00 - 9pm",
+    backgroundImg: "/side-events/solana-mixer.jpg",
   },
 ];

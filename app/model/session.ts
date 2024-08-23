fdd71ed831a193b2df6bad14f53bf77e12f4ad12
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-08-24 01:20:57 +0200

    change: improve agenda pages and session model

--- app/model/session.ts
+++ app/model/session.ts
@@ -1,12 +1,29 @@
+import Speaker from "@/app/model/speaker";
+
+export const Tracks = [
+  "Education Track",
+  "Application Track",
+  "Research Track",
+  "Regulation Track",
+  "Ecosystem Track",
+] as const;
+
+export const Stages = [
+  "Dome Stage",
+  "Tech Stage",
+  "Education Stage",
+  "Workshop Stage",
+] as const;
+
 export interface Session {
   title: string;
-  track: string;
-  type: string;
+  track: (typeof Tracks)[number];
+  type: "Workshop" | "Panel Discussion" | "Talk";
   startTime: string;
   endTime: string;
-  room: string;
+  room: (typeof Stages)[number];
   description: string;
-  speakers: string[];
+  speakers: Speaker[];
 }
 
 export default Session;

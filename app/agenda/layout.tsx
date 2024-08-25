1a69c0506aba4d2266948214cdd074053944e0bf
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-08-25 20:18:04 +0200

    fix: removed layout.tsx

--- app/agenda/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { useSession } from "../hooks/useSession";
-import Agenda from "./page";
-
-export default async function AgendaLayout() {
-  const sessions = await useSession();
-
-  return <Agenda sessions={sessions} />;
-}

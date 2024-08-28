bc5a42e50847a940af1a233f0e4092e8408c5460
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-08-29 01:21:10 +0200

    change: improve mobile website

--- app/agenda/agenda.tsx
+++ app/agenda/agenda.tsx
@@ -37,7 +37,7 @@ export const Agenda: React.FC<AgendaProps> = ({ sessions }) => {
     <div className={"flex flex-col md:flex-row relative gap-8 mt-24"}>
       <div
         id="filter"
-        className="md:sticky top-24 border border-white p-6 md:min-w-[250px] flex flex-col gap-6 h-fit"
+        className="bg-black sticky top-[76px] md:top-24 border border-white p-6 md:min-w-[250px] flex flex-col gap-4 md:gap-6 h-fit"
       >
         <Text textType={"sub_title"} className="text-left" as="p">
           Filter
@@ -46,7 +46,7 @@ export const Agenda: React.FC<AgendaProps> = ({ sessions }) => {
           <Text textType={"paragraph"} className="font-bold text-left" as="p">
             Days
           </Text>
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-row md:flex-col gap-2">
             {[new Date("2024-09-12"), new Date("2024-09-13")].map(
               (date, index) => (
                 <Toggle
@@ -68,58 +68,65 @@ export const Agenda: React.FC<AgendaProps> = ({ sessions }) => {
                   >
                     {date.toLocaleDateString("en-DE", {
                       weekday: "long",
-                      day: "numeric",
-                      month: "short",
-                      year: "numeric",
                     })}
+                    <span className="hidden md:inline">
+                      ,{" "}
+                      {date.toLocaleDateString("en-DE", {
+                        day: "numeric",
+                        month: "short",
+                        year: "numeric",
+                      })}
+                    </span>
                   </Text>
                 </Toggle>
               ),
             )}
           </div>
         </div>
-        <div className="flex flex-col gap-3">
-          <Text textType={"paragraph"} className="font-bold text-left" as="p">
-            Stages
-          </Text>
-          <div className="flex md:flex-col flex-wrap gap-2">
-            <Select.Root
-              onValueChange={(value: (typeof Stages)[number] & "all") => {
-                setStageFilter(value);
-              }}
-            >
-              <Select.Trigger placeholder={"Any Stage"} className="w-full" />
-              <Select.Content>
-                <Select.Item value={"all"}>Any Stage</Select.Item>
-                {Stages.map((stage, index) => (
-                  <Select.Item value={stage} key={index}>
-                    {stage}
-                  </Select.Item>
-                ))}
-              </Select.Content>
-            </Select.Root>
+        <div className="flex flex-row gap-6 md:flex-col w-full">
+          <div className="flex flex-col gap-3">
+            <Text textType={"paragraph"} className="font-bold text-left" as="p">
+              Stages
+            </Text>
+            <div className="flex md:flex-col flex-wrap gap-2">
+              <Select.Root
+                onValueChange={(value: (typeof Stages)[number] & "all") => {
+                  setStageFilter(value);
+                }}
+              >
+                <Select.Trigger placeholder={"Any Stage"} className="w-full" />
+                <Select.Content>
+                  <Select.Item value={"all"}>Any Stage</Select.Item>
+                  {Stages.map((stage, index) => (
+                    <Select.Item value={stage} key={index}>
+                      {stage}
+                    </Select.Item>
+                  ))}
+                </Select.Content>
+              </Select.Root>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-3">
-          <Text textType={"paragraph"} className="font-bold text-left" as="p">
-            Tracks
-          </Text>
-          <div className="flex md:flex-col flex-wrap gap-2">
-            <Select.Root
-              onValueChange={(value: (typeof Tracks)[number] & "all") => {
-                setTrackFilter(value);
-              }}
-            >
-              <Select.Trigger placeholder={"Any Track"} className="w-full" />
-              <Select.Content>
-                <Select.Item value={"all"}>Any Track</Select.Item>
-                {Tracks.map((stage, index) => (
-                  <Select.Item value={stage} key={index}>
-                    {stage}
-                  </Select.Item>
-                ))}
-              </Select.Content>
-            </Select.Root>
+          <div className="flex flex-col gap-3">
+            <Text textType={"paragraph"} className="font-bold text-left" as="p">
+              Tracks
+            </Text>
+            <div className="flex md:flex-col flex-wrap gap-2">
+              <Select.Root
+                onValueChange={(value: (typeof Tracks)[number] & "all") => {
+                  setTrackFilter(value);
+                }}
+              >
+                <Select.Trigger placeholder={"Any Track"} className="w-full" />
+                <Select.Content>
+                  <Select.Item value={"all"}>Any Track</Select.Item>
+                  {Tracks.map((stage, index) => (
+                    <Select.Item value={stage} key={index}>
+                      {stage}
+                    </Select.Item>
+                  ))}
+                </Select.Content>
+              </Select.Root>
+            </div>
           </div>
         </div>
       </div>

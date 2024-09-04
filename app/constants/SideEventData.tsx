14d8e5d647ce0269d7b8351c8d7fb330382a08c7
Lucas Aschenbach <Lucas.Aschenbach@gmail.com>
2024-09-04 02:51:57 +0200

    Update times for side events

--- app/constants/SideEventData.tsx
+++ app/constants/SideEventData.tsx
@@ -7,7 +7,7 @@ export const sideEventsItems = [
     description:
       "This hybrid hackathon invites developers, innovators, and tech enthusiasts to create cutting-edge solutions using AI-powered chatbots, oracles, and state channels on the Æternity blockchain. Participants will use Sophia, Æternity’s smart contract language, to build decentralized applications (dApps) that address real-world challenges. Whether participating online or in-person, teams will collaborate and compete to develop impactful solutions.",
     link: "https://www.eventbrite.co.uk/e/ternity-uni-hack-shaping-the-future-with-ai-oracles-state-channels-tickets-1005566675997?aff=oddtdtcreator",
-    date: "Tuesday, September 10 | 9:30am - Friday, September 13 | 9pm",
+    date: "Tuesday, September 10 | 9:30 - Friday, September 13 | 21:00",
     backgroundImg: "/side-events/aeternity-unihack.jpg",
   },
   {
@@ -17,7 +17,7 @@ export const sideEventsItems = [
     description:
       "​Join us for our TUM Blockchain Conference side event together with Aachen Blockchain Club.",
     link: "https://lu.ma/rt4yg9gp",
-    date: "Wednesday, September 11 | 6:00 - 9pm",
+    date: "Wednesday, September 11 | 18:00 - 21:00",
     backgroundImg: "/side-events/solana-mixer.jpg",
   },
   {
@@ -27,7 +27,7 @@ export const sideEventsItems = [
     description:
       'Ready to kick off the TUM Blockchain Conference 2024 with a bang? Join us at our lively pre-event, "AI, Privacy & DePin in Web3" September 11th at Wayra Germany in the heart of Munich for an evening filled with insightful discussions, networking, and a whole lot of heartfelt welcome vibes.',
     link: "https://lu.ma/v24yqx2q",
-    date: "Wednesday, September 11 | 6:00 - 9pm",
+    date: "Wednesday, September 11 | 18:00 - 21:00",
     backgroundImg: "/side-events/pre-event.jpg",
   },
   {
@@ -37,7 +37,7 @@ export const sideEventsItems = [
     description:
       "Join us for another exciting w3.pizza event as we kick off TUM Blockchain Conference with KPMG & Solana. This is your chance to learn more about Digital Assets and hearing the latest insights from industry experts such as Staking Facilities, Solana Beach, Arcium, TUM Blockchain Club & Aachen Blockchain Club",
     link: "https://lu.ma/vbkdwhxe",
-    date: "Wednesday, September 11 | 6:30 - 10pm",
+    date: "Wednesday, September 11 | 18:30 - 21:00",
     backgroundImg: "/side-events/w3-pizza.jpg",
   },
   {
@@ -47,7 +47,7 @@ export const sideEventsItems = [
     description:
       "XRPL Meetups aim to spread knowledge and create real-life connections and communities around blockchain and the XRP Ledger by establishing local “XRPL Hubs” across Europe. These meetups provide regular gatherings where individuals from various sectors can meet, exchange ideas, and deepen their understanding of blockchain technology. Designed to be inclusive, XRPL Meetups welcome participants from academia, private industries, public sectors, and NGOs.",
     link: "https://www.eventbrite.fr/e/xrpl-meetup-in-munich-tickets-957654278727",
-    date: "Thursday, September 12 | 5:30 - 9pm",
+    date: "Thursday, September 12 | 17:30 - 21:00",
     backgroundImg: "/side-events/xrpl-meetup.jpg",
   },
   {
@@ -57,7 +57,7 @@ export const sideEventsItems = [
     description:
       "Join us for a relaxed evening at the Blockchain Ecosystem Night, hosted by the TUM Blockchain Club, Blockchain Bayern, and Bundesblock. We'll kick off with two panels: one featuring students from international blockchain clubs and the other highlighting local blockchain initiatives. In between, Prof. Möslein will present his new book. After that, we’ll come together for drinks and some casual networking. It's the perfect chance to connect with other blockchain enthusiasts and experts. Looking forward to seeing you there!",
     link: "https://www.eventbrite.de/e/blockchain-ecosystem-night-tickets-1005031866367?aff=oddtdtcreator",
-    date: "Thursday, September 12 | 5:45 - 8pm",
+    date: "Thursday, September 12 | 17:45 - 20:00",
     backgroundImg: "/side-events/ecosystem-night.jpg",
   },
   {
@@ -67,7 +67,7 @@ export const sideEventsItems = [
     description:
       "Join us for the Sui Community Breakfast on September 13, 2024, from 8:30 to 10:30 AM at the Rosi Coffee & Bar in München. ​Get the latest news about the Sui ecosystem, engage in insightful discussions, enjoy delicious food and drinks, and connect with other Sui builders and developers — just a 5-minute walk from the TUM Blockchain Conference venue. ​Everyone is welcome, whether you're a developer, project founder, investor, or Web3 enthusiast.",
     link: "https://lu.ma/zs91488v?tk=GlGfDy",
-    date: "Friday, September 13 | 8:30 - 10.30am",
+    date: "Friday, September 13 | 8:30 - 10:30",
     backgroundImg: "/side-events/community-breakfast.jpg",
   },
   {
@@ -77,7 +77,7 @@ export const sideEventsItems = [
     description:
       "Join us for an exciting evening where 15 initiatives will pitch the most interesting projects that they have been working on and how you can get involved with them! Note: This event is open to everyone attending the conference, so whether you're a professional, entrepreneur, or corporate, don't miss joining us to get a scoop on the latest happenings!",
     link: "/student-initiatives-night",
-    date: "Friday, September 13 | 5 - 9pm",
+    date: "Friday, September 13 | 17:00 - 21:00",
     backgroundImg: "/side-events/student-initiatives-night.jpg",
   },
 ];

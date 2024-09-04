7e2caa59385ffdb4703bbd78d2b1f4057cadd191
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-09-04 17:03:49 +0200

    fix: missing paginating the request to contentful

--- app/hooks/useSpeaker.ts
+++ app/hooks/useSpeaker.ts
@@ -15,35 +15,52 @@ export const useSpeaker = async (count?: number): Promise<Speaker[]> => {
   const url = new URL(
     `https://cdn.contentful.com/spaces/${spaceId}/environments/${environment}/entries`,
   );
-  url.search = new URLSearchParams({
-    content_type: "speaker",
-    access_token: process.env.CONTENTFUL_ACCESS_TOKEN!,
-    order: "fields.priority,fields.name",
-    ...(count && { limit: count + "" }),
-  }).toString();
 
-  const res = await fetch(url, {
-    method: "GET",
-    headers: {
-      Authorization: process.env.CONTENTFUL_ACCESS_TOKEN!,
-    },
-  });
+  let speakers: Speaker[] = [];
+  let skipCounter = 0;
 
-  const jsonRes = await res.json();
+  do {
+    url.search = new URLSearchParams({
+      content_type: "speaker",
+      access_token: process.env.CONTENTFUL_ACCESS_TOKEN!,
+      order: "fields.priority,fields.name",
+      skip: skipCounter + "",
+      ...(count && { limit: count + "" }),
+    }).toString();
 
-  const speakers = jsonRes.items?.map((item: any): Speaker => {
-    return {
-      name: item.fields.name,
-      description: item.fields.description,
-      profilePhoto: findAssetURL(
-        jsonRes.includes.Asset,
-        item.fields.profilePhoto.sys.id,
-      ),
-      url: item.fields.url,
-      urlType: item.fields.urlType,
-      priority: item.fields.priority,
-    };
-  });
+    const res = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: process.env.CONTENTFUL_ACCESS_TOKEN!,
+      },
+    });
+
+    const jsonRes = await res.json();
+    const { total } = jsonRes;
+
+    speakers = speakers.concat(
+      jsonRes.items?.map((item: any): Speaker => {
+        return {
+          name: item.fields.name,
+          description: item.fields.description,
+          profilePhoto: findAssetURL(
+            jsonRes.includes.Asset,
+            item.fields.profilePhoto.sys.id,
+          ),
+          url: item.fields.url,
+          urlType: item.fields.urlType,
+          priority: item.fields.priority,
+        };
+      }),
+    );
+
+    // Stop iterating if the current response count is not bounded by maximum (100)
+    if (total === 0) {
+      break;
+    } else {
+      skipCounter += 100;
+    }
+  } while (!count);
 
   if (!speakers) {
     console.log("Speakers is undefined");

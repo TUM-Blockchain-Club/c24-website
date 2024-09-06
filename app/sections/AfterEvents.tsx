9c9fdc7ef8e5bec628649150585734e95cb198fb
KameterLuis <contact@luiskameter.com>
2024-09-06 16:40:40 +0200

    [Edit] Add speaker socials

--- app/sections/AfterEvents.tsx
+++ app/sections/AfterEvents.tsx
@@ -13,14 +13,14 @@ const AfterEvents = () => {
         <div className="lg:flex lg:space-x-4 space-y-4 lg:space-y-0">
           <AfterEvent
             title="Blockchain Ecosystem Night"
-            date="12.09.2024"
+            date="12.09.2024 | 5.45 - 8pm"
             img="/side-events/ecosystem-night.jpg"
             link="https://www.eventbrite.de/e/blockchain-ecosystem-night-tickets-1005031866367?aff=oddtdtcreator"
             desc="Join us for a relaxed evening at the Blockchain Ecosystem Night, hosted by the TUM Blockchain Club, Blockchain Bayern, and Bundesblock. We'll kick off with two panels: one featuring students from international blockchain clubs and the other highlighting local blockchain initiatives. In between, Prof. Möslein will present his new book. After that, we’ll come together for drinks and some casual networking. It's the perfect chance to connect with other blockchain enthusiasts and experts. Looking forward to seeing you there!"
           />
           <AfterEvent
             title="Student Initiatives Night"
-            date="13.09.2024"
+            date="13.09.2024 | 8.30 - 10.30pm"
             img="/side-events/student-initiatives-night.jpg"
             link="/student-initiatives-night"
             desc="Student initiatives are an integral part of Munich's innovative

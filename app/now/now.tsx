ba8e0e8f3e831c9b94daae7db0c6ad8961948c21
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-09-08 15:03:35 +0200

    fix: change environment variable value

--- app/now/now.tsx
+++ app/now/now.tsx
@@ -53,7 +53,7 @@ const Now: React.FC<NowProps> = ({ sessions, simulatedDate }) => {
     sessionsInStage: Session[],
   ): { currentSession: Session | null; nextSession: Session | null } => {
     const now =
-      simulatedDate && process.env.NEXT_PUBLIC_PRODUCTION !== "production"
+      simulatedDate && process.env.NEXT_PUBLIC_PRODUCTION !== "1"
         ? currentTime
         : new Date();
     let currentSession = null;

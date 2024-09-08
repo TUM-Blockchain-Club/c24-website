aa806129b760174982ad8f9d9de539cb1f6690af
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-09-08 14:41:38 +0200

    change: simulated time is from 15 minute before event start

--- app/now/page.tsx
+++ app/now/page.tsx
@@ -5,6 +5,6 @@ export default async function NowPage() {
   const sessions = await useSession();
 
   return (
-    <Now sessions={sessions} simulatedDate={new Date("2024-09-12T11:00:00")} />
+    <Now sessions={sessions} simulatedDate={new Date("2024-09-12T09:15:00")} />
   );
 }

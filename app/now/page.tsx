b38e122f8c4f0ea5681a42dbd9bc96947ad7295c
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-09-08 14:46:04 +0200

    fix: corrected timezone

--- app/now/page.tsx
+++ app/now/page.tsx
@@ -5,6 +5,9 @@ export default async function NowPage() {
   const sessions = await useSession();
 
   return (
-    <Now sessions={sessions} simulatedDate={new Date("2024-09-12T09:15:00")} />
+    <Now
+      sessions={sessions}
+      simulatedDate={new Date("2024-09-12T09:00:00+02:00")}
+    />
   );
 }

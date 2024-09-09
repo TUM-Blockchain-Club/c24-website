0be852a771eb01ba333c37826119e82eb9e8f1ed
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2024-09-09 23:35:08 +0200

    fix: unoptimized image

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   generateBuildId: async () => {
     return process.env.VERCEL_DEPLOYMENT_ID || Date.now().toString();
   },

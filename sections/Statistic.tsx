5456e15387cb58ce485821434413affd538b0113
Xiyue Zhang <phoebez_99@outlook.com>
2024-11-01 19:13:28 +0100

    [update] update stats center alignment
    update stats center alignment

--- sections/Statistic.tsx
+++ sections/Statistic.tsx
@@ -12,37 +12,37 @@ const Statistic = () => {
           This Year Statistic
         </Text>
         <div className={"flex flex-col items-center lg:flex-row gap-12 mt-12"}>
-          <div className={"flex flex-col gap-2 items-center lg:items-start"}>
+          <div className={"flex flex-col gap-2 items-center lg:items-center"}>
             <Text className={""} textType={"sub_hero"}>
               1000+
             </Text>
             <Text textType={"sub_title"}>Attendees</Text>
           </div>
-          <div className={"flex flex-col gap-2 items-center lg:items-start"}>
+          <div className={"flex flex-col gap-2 items-center lg:items-center"}>
             <Text className={""} textType={"sub_hero"}>
               150+
             </Text>
             <Text textType={"sub_title"}>Speakers</Text>
           </div>
-          <div className={"flex flex-col gap-2 items-center lg:items-start"}>
+          <div className={"flex flex-col gap-2 items-center lg:items-center"}>
             <Text className={""} textType={"sub_hero"}>
               23
             </Text>
             <Text textType={"sub_title"}>Sponsors</Text>
           </div>
-          <div className={"flex flex-col gap-2 items-center lg:items-start"}>
+          <div className={"flex flex-col gap-2 items-center lg:items-center"}>
             <Text className={""} textType={"sub_hero"}>
-              84
+              83
             </Text>
             <Text textType={"sub_title"}>Talks</Text>
           </div>
-          <div className={"flex flex-col gap-2 items-center lg:items-start"}>
+          <div className={"flex flex-col gap-2 items-center lg:items-center"}>
             <Text className={""} textType={"sub_hero"}>
               20
             </Text>
             <Text textType={"sub_title"}>Panels</Text>
           </div>
-          <div className={"flex flex-col gap-2 items-center lg:items-start"}>
+          <div className={"flex flex-col gap-2 items-center lg:items-center"}>
             <Text className={""} textType={"sub_hero"}>
               7
             </Text>
